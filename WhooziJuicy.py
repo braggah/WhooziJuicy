c183453c3740dbe9ca6d0b26a0c2da35035b2883
Glen Braggah Mudau <glenmudau@outlook.com>
2016-04-07 10:09:36 +0200

    Added files via upload
    Finished coding for the assignmen

--- WhooziJuicy.py
+++ WhooziJuicy.py
@@ -1,47 +1,76 @@
+#..............................................................................................
+# Name: WhooziJuicy App
+# Purpose :For Junior Software position abilities assessment
+# Author: Mudau Glen
+# Created: 04/04/2016
+# Copyright: (c) Glen Mudau 2016
+
+#.......................................................................................................
+
 from tkinter import *
 import tkinter.messagebox
 
+
 class Person:
 
-    def __init__(self, form):
-        frame = Frame(form)
-        frame.pack()
 
-        self.checkAgeButton = Button(frame, text="Entry Status", command=self.checkAge())
-        self.checkAgeButton.pack(side=RIGHT)
-        self.checkUsername = Button(frame, text="Check user", command=self.checkuser())
-        self.checkAgeButton.pack(side=RIGHT)
-        self.quitButton = Button(frame, text="Exit", command=frame.quit)
-        self.quitButton.pack(side=LEFT)
+    def __init__(self, master):
+
+        self.userName = StringVar()
+        self.ageUser = IntVar()
+
+        self.master = master
+        master.title("The WhooziJuicy App")
+        master.geometry('400x200+100+200')
+
+        self.label1 = Label(master, text='Hello, and Welcome to WhooziJuicy Night Club ', fg="blue").grid(row=0, column=1)
+        self.label2 = Label(master, text="Enter Username").grid(row=2, column=0)
+        self.label2 = Label(master, text="Enter Age (remove 0 first)").grid(row=3, column=0)
+
+        self.usernameTextbox = Entry(master, textvariable=self.userName).grid(row=2, column=1)
+        self.ageTextbox = Entry(master, textvariable=self.ageUser).grid(row=3, column=1)
+
+        self.checkAgeButton = Button(master, text="Check entry status", command=self.checkAge).grid(row=4, column=1)
+        self.checkUserButton = Button(master, text="Check True age", fg="red", command=self.checkUser).grid(row=4, column=0)
+
+
+    def checkUser(self):
+        self.username = self.userName.get()
+        if self.username == "Q24":
+            X = 24 + 6
+            return tkinter.messagebox.showinfo("Q24 True age", X)
+        else:
+            return tkinter.messagebox.showinfo("User", "Telling the truth")
+
 
     def checkAge(self):
-        username = input("Enter username: ")
-        age = int(input("Enter age: "))
-        if age >= 18:
-            print(username, "Is granted access and")
-            if age < 90:
-                if age <= 50:
-                    print(username, "Must stand on que")
-                else:
-                    print(username, "Must get in first")
-            else:
-                print(username, "Is denied access!, too old/Invalid")
 
-        elif age < 0:
-            print(username, "Has entered invalid age")
+        self.username = self.userName.get()
+        self.age = self.ageUser.get()
+
+        if self.age >= 18:
+            if self.age < 90:
+                if self.age <= 50:
+                    return tkinter.messagebox.showinfo(self.username, "Acess Granted but Must stand on the Queue")
+                else:
+                    return tkinter.messagebox.showinfo(self.username, "Acess Granted, Is an elder, Must get in First")
+            else:
+                return tkinter.messagebox.showinfo(self.username, "Is Denied Acess, too old")
+
+        elif self.age < 0:
+            return tkinter.messagebox.showinfo(self.username, "Has entered invalid age")
 
         else:
-            tkinter.messagebox.showinfo(username, "Is denied access - (Under Age)")
+            return tkinter.messagebox.showinfo(self.username, "Is under age!, and Denied Access")
 
-    def checkuser(self, X):
-        if username == "Q24":
-            self.X = 24 + 6
-            return int(self.X)
 
 root = Tk()
-tkinter.messagebox.showinfo("WhooziJuicy App")
-thePerson = Person(root)
+User = Person(root)
 root.mainloop()
+
+
+
+
 
 
 
